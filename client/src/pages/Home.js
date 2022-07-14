3bf1c57bb35833e38b1a09b68c3f7b0b9ff25da0
Alex Hageman <ahageman27@gmail.com>
2022-07-14 13:51:23 -0600

    basic search page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,6 +3,7 @@ import React from "react";
 const Home = () => {
 	return (
     	<div className="container">
+			<h2>HOME</h2>
     	</div>
   	);
 };
--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -31,3 +31,24 @@ export const ADD_USER = gql`
     }
   }
 `;
+
+export const SEARCH = gql`
+  mutation addUser(
+    $firstName: String!
+    $lastName: String!
+    $email: String!
+    $password: String!
+  ) {
+    addUser(
+      firstName: $firstName
+      lastName: $lastName
+      email: $email
+      password: $password
+    ) {
+      token
+      user {
+        _id
+      }
+    }
+  }
+`;
