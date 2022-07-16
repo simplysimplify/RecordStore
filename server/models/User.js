18d8bbc2fcc9d10f3bf76ac73495b5378a6a6d54
Alex B <acbird.ab@gmail.com>
2022-07-16 16:13:53 -0600

    working on user authentication/login

--- server/models/User.js
+++ server/models/User.js
@@ -1,5 +1,7 @@
 const { Schema, model } = require("mongoose");
 
+const bcrypt = require("bcrypt");
+
 const userSchema = new Schema({
   username: {
     type: String,
@@ -29,6 +31,19 @@ const userSchema = new Schema({
   //    virtuals: true,
   //   },
 });
+
+userSchema.pre("save", async function (next) {
+  if (this.isNew || this.isModified("password")) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+
+  next();
+});
+
+userSchema.methods.isCorrectPassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
 
 const User = model("User", userSchema);
 
--- server/server.js
+++ server/server.js
@@ -3,10 +3,7 @@ const { ApolloServer } = require("apollo-server-express");
 const path = require("path");
 const { typeDefs, resolvers } = require("./schemas");
 const db = require("./config/connection");
-// const { authMiddleware } = require("./utils/auth");
-
-console.log(typeDefs);
-console.log(resolvers);
+const { authMiddleware } = require("./utils/auth");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -14,7 +11,7 @@ const app = express();
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  // context: authMiddleware,
+  context: authMiddleware,
 });
 
 app.use(express.urlencoded({ extended: false }));
