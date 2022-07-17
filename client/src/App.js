32dc8a0109ee39fee7bcf2405d81a16525de6537
Sev <simplysimplifyy@gmail.com>
2022-07-17 09:53:47 -0600

    syntax changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,56 +1,54 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  createHttpLink,
+ ApolloClient,
+ InMemoryCache,
+ ApolloProvider,
+ createHttpLink,
 } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
 
-import Home from './pages/Home';
 import Login from "./pages/login";
 import Signup from "./pages/signup";
 import Search from "./pages/Search";
 import Results from "./pages/results";
 
-
-import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 const httpLink = createHttpLink({
-  uri: "/graphql",
+ uri: "/graphql",
 });
 
 const authLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem("id_token");
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : "",
-    },
-  };
+ const token = localStorage.getItem("id_token");
+ return {
+  headers: {
+   ...headers,
+   authorization: token ? `Bearer ${token}` : "",
+  },
+ };
 });
 
 const client = new ApolloClient({
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache(),
+ link: authLink.concat(httpLink),
+ cache: new InMemoryCache(),
 });
 
 function App() {
-  return (
-    <ApolloProvider client={client}>
-      <Router>
-        <div>
-          <Routes>
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/" element={<Search />} />
-            <Route path="/results" element={<Results />} />
-          </Routes>
-        </div>
-      </Router>
-    </ApolloProvider>
-  );
+ return (
+  <ApolloProvider client={client}>
+   <Router>
+    <div>
+     <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/" element={<Search />} />
+      <Route path="/results" element={<Results />} />
+     </Routes>
+    </div>
+   </Router>
+  </ApolloProvider>
+ );
 }
 
 export default App;
--- server/server.js
+++ server/server.js
@@ -8,39 +8,37 @@ const db = require("./config/connection");
 console.log(typeDefs);
 console.log(resolvers);
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  // context: authMiddleware,
+ typeDefs,
+ resolvers,
+ // context: authMiddleware,
 });
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
+ app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+ res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async (typeDefs, resolvers) => {
-  await server.start();
-  server.applyMiddleware({ app });
+ await server.start();
+ server.applyMiddleware({ app });
 
-  db.once("open", () => {
-    app.listen(PORT, () => {
-      console.log(`API server running on port ${PORT}!`);
-      console.log(
-        `Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`
-      );
-    });
+ db.once("open", () => {
+  app.listen(PORT, () => {
+   console.log(`API server running on port ${PORT}!`);
+   console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
   });
+ });
 };
 
 // Call the async function to start the server
